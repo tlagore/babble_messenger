99b2c15fdb29223f61aea73d09445fdb58ee13ad
tyrone.lagore <tyrone.lagore@zone45-ec.cs.ucalgary.ca>
2017-03-12 19:22:01 -0600

    initial import - ui in reasonable condition

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+http.listen( port, function () {
+    console.log('listening on port', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// listen to 'chat' messages
+io.on('connection', function(socket){
+    socket.on('chat', function(msg){
+	io.emit('chat', msg + " u fuk");
+    });
+});
