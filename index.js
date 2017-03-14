5c5fe8a096aa242d1d72d3a4d17c882999d2302e
tyrone.lagore <tyrone.lagore@zone45-eb.cs.ucalgary.ca>
2017-03-13 22:17:59 -0600

    Sorry, way too long in between commits. Added users being assigned names which remain persistent between sessions. Users see whe other users join/leave the server and can message each other. User list is populated with users when they join and they are removed when they disconnect. The message log is populated when a user logs in. In the process of adding command line arguments like /nick and such.

--- index.js
+++ index.js
@@ -1,32 +1,157 @@
 var express = require('express');
+var cookie_parser = require('cookie-parser');
+var socketIoCookieParser = require("socket.io-cookie-parser");
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+app.use(cookie_parser());
+io.use(socketIoCookieParser());
+
+var users = {};
+var taken_names = {};
+var message_log = [];
 
 http.listen( port, function () {
     console.log('listening on port', port);
 });
 
 
+
 app.get("/", function(req, res, next){
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(ip);
+   // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var cookie = req.cookies.user_hash;
+    console.log(cookie);
+    
+    if (cookie === undefined){
+	let user_hash = Math.floor(Math.random() * 1000000);
+
+	name = generate_username();
+
+	users[user_hash] = { 'user_name': name, 'user_color': '#00ace6' };
+	console.log("new user: " + name);
+	//1 hour expiration on cookie
+	res.cookie("user_hash", user_hash, { maxAge: 1000 * 60 * 60, httpOnly: true});
+    }else{
+	if (!(cookie in users)){
+	    console.log("Cookie wasnt in users...");
+	    users[cookie] = { 'user_name': generate_username(), 'user_color': '#00ace6' };
+	}
+	console.log("existing user: " + cookie);
+    }
     next();
 });
 
 app.use(express.static(__dirname + '/public'));
 
-/*app.get("/", function(req, res){
-    console.log("in here.");
-});*/
-
 // listen to 'chat' messages
 io.on('connection', function(socket){
+    let user = socket.request.cookies.user_hash;
+
+    if(user != undefined){
+	console.log("Known user: " + user);
+    }else{
+	console.log("New user");
+    }
+
+    socket.on('loaded', function(){
+	//console.log(socket.id);
+	console.log(users);
+	if(users[user] != undefined){
+	    users[user].connected = true;
+	}else{
+	    console.log("Undefined...");
+	    users[user] = {'name': generate_username(),
+			   'user_color':'#00ace6',
+			   'connected':true};
+	}
+	
+	socket.emit('user_name', users[user].user_name);
+
+	message = {'user' : 'server',
+		   'timestamp':new Date().getTime(),
+		   'color': '#76d65e',
+		   'contents': users[user].user_name + " has joined the server."};
+
+	io.emit('chat', message);
+
+	for(let message of message_log){
+	    socket.emit('chat', message);
+	}
+
+	for(let key in users){
+	    if (users[key].connected){
+		io.emit('user_joined', users[key].user_name);
+	    }
+	}
+    });
+
+    socket.on('disconnect', function(msg){
+	if (users[user] != undefined){
+	    let message = { 'user': 'server',
+			    'timestamp':new Date().getTime(),
+			    'color': '#e24646',
+			    'contents':users[user].user_name + " has left the server." };
+	    
+	    users[user].connected = false;
+	    
+	    io.emit('chat', message);
+	    io.emit('user_left', users[user].user_name);
+	}		     
+    });
+    
     socket.on('chat', function(msg){
 	let date = new Date();
-	io.emit('chat', { 'user' : 'Bob',
+	let message = { 'user' : users[user].user_name,
+			'timestamp': date.getTime(),
+			'color': users[user].user_color,
+			'contents' : msg };
+
+
+	if (message_log.length >= 300){
+	    shift(message_log);
+	}
+
+	message_log.push(message);
+	
+	io.emit('chat', { 'user' : users[user].user_name,
 			  'timestamp': date.getTime(),
 			  'contents' : msg });
     });
+
 });
+
+
+/*
+  Attempt to generate a name from preset names. If 20 iterations occur without a name chosen,
+  the current name will be given, appended by 3 random numbers
+*/
+function generate_username(){
+    let first_names = ['skilled', 'willful', 'angry', 'pretty', 'standard', 'plain', 'princess', 'prince', 'butternut'];
+    let middle_names = ['toad', 'hamster', 'jock', 'skillet', 'cake', 'pie', 'rock', 'turtle', 'cube', 'pork', 'panzy'];
+    let last_names = ['herder', 'lumberjack', 'smither', 'miner', 'clown', 'baker', 'chef', 'teacher'];
+    let i = 0;
+    let name = undefined;
+
+    first = first_names[Math.floor(Math.random() * first_names.length)];
+    middle = middle_names[Math.floor(Math.random() * middle_names.length)];
+    last = last_names[Math.floor(Math.random() * last_names.length)];
+
+    name = first + "_" + middle + "_" + last;
+
+    while (i < 20 && (name in taken_names)){
+	first = first_names[Math.floor(Math.random() * first_names.length)];
+	middle = middle_names[Math.floor(Math.random() * middle_names.length)];
+	last = last_names[Math.floor(Math.random() * last_names.length)];
+	
+	name = first + "_" + middle + "_" + last;
+	i++;
+    }
+
+    if(i == 20){
+	name += Math.random().toString().substring(2, 5);
+    }
+
+    return name;
+}
--- public/client.js
+++ public/client.js
@@ -1,16 +1,36 @@
 
 // document ready calls
 $(function(){
-   var socket = io();
+    var socket = io();
+    socket.emit('loaded');
+
     
     socket.on('chat', function(msg){
 	user = msg.user;
 	timestamp = msg.timestamp;
 	message = msg.contents;
+	color = msg.color;
+	
+	let formatted_message = generate_message(user, new Date(timestamp).toUTCString(), message, timestamp, color);
 
-	let formatted_message = generate_message(user, new Date(timestamp).toUTCString(), message, timestamp);
 	$('#messages').prepend(formatted_message);
 	display_message(user, timestamp);
+    });
+
+    socket.on('user_joined', function(name){
+	if(!$('#'+name).length){
+	    $('#users').append('<h4 id=' + name + '>' + name + '</h4>');
+	}
+    });
+
+    socket.on('user_left', function(name){
+	if($('#'+name).length){
+	    $('#'+name).remove();
+	}
+    });
+
+    socket.on('user_name', function(name){
+	$('#whoami').html("You are: <b>" + name + "</b>");
     });
     
     $('#input-msg').keypress(function(event){
@@ -19,21 +39,68 @@ $(function(){
 	    $('#input-msg').val('');
 	}else if(event.which == 13){
 	    let msg = $('#input-msg').val();
-	    if(msg != ''){
-		$('#input-msg').val('');
+
+	    let message = parse_message(msg);
+	    if(message != undefined){
 		socket.emit('chat', msg);
-	    }	    
+	    }
+
+	    $('#input-msg').val('');
 	}
     });
     
     $('#submit-message').click(function(){
 	let msg = $('#input-msg').val()
-	if (msg != ''){
-	    socket.emit('chat', msg);
+
+	let message = parse_message(msg);
+	if (message != undefined){
+	    socket.emit(parse_message(message));
 	}
+
+	$('#input-msg').val('');
     });
 });
 
+
+function parse_message(msg){
+    message = undefined;
+    if (msg[0] = '/'){
+	msgParts = msg.split(" ");
+	message = parse_command(msgParts);
+    }
+    else if (msg == ''){
+	message = undefined;
+    }else{
+	message = msg;
+    }
+    
+    return message;;
+}
+
+function parse_command(msgParts){
+    msg = undefined;
+    
+    if (msgParts[0] === "/nick"){
+	newNick = msgParts[1];
+	if (newNick != undefined){
+	    //check for consistency
+	    alert("yo");
+	    msg = msg[1];
+	}else{
+	    timestamp = new Date().getTime();
+	    formatted_message = generate_message(
+		"server",
+		undefined,
+		"Invalid usage, correct usage is /nick [new_nickname]",
+		timestamp,
+		"#e24646");
+	    $('#messages').prepend(formatted_message);
+	    display_message("server", timestamp);
+	}
+    }
+
+    return msg;
+}
 
 /* functions */ 
 function display_message(user, timestamp){
@@ -44,17 +111,21 @@ function display_message(user, timestamp){
     });
 }
 
-function generate_message(user, timestamp, msg, utc){
-    let mesg = '<div id=' + user + utc + 
+function server_message(utc, msg, color){
+    return mesg = '<div id=server' + utc + 
 	' style="opacity:0.0;" class="message">' +
-	'<div class="message-header">' +
-	'<div class="message-user">' + user + '</div>' + 
-	'<div class="message-time">' + timestamp + '</div>' +
-	'</div><div class="message-content">' + msg + '</div></div>'
-    
-    return mesg;
+	'<div class="message-content" style="color:' + color +'">' + msg + '</div></div>'
 }
 
-function recv_message(msg, callback){
-    
+function generate_message(user, timestamp, msg, utc, color){
+    if (user == "server"){
+	return server_message(utc, msg, color)
+    }else{
+	return '<div id=' + user + utc + 
+	    ' style="opacity:0.0;" class="message">' +
+	    '<div class="message-header">' +
+	    '<div class="message-user" style="color: ' + color + '">' + user + '</div>' + 
+	    '<div class="message-time">' + timestamp + '</div>' +
+	    '</div><div class="message-content">' + msg + '</div></div>'
+    }
 }
